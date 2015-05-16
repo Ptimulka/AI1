ccbdb56517146674a4fa636793cf556b1bf2b2e4
Łukasz <vaenyr@gmail.com>
2015-05-16 15:00:30 +0200

    C.d.

--- src/ann/fann_irprop_logical.cpp
+++ src/ann/fann_irprop_logical.cpp
@@ -3,18 +3,133 @@
 #include <algorithm>
 using namespace std;
 
-void * FanniRPROP::createSession()
+const double& as_double(unsigned long long const& mem)
 {
-    return initSession();
+    return *(const double* const)&mem;
 }
 
-void FanniRPROP::createNewNet(initializer_list<uint> const & layers)
+double& as_double(unsigned long long& mem)
+{
+    return *(double* const)&mem;
+}
+
+const unsigned long long& as_ull(double const& val)
+{
+    return *(const unsigned long long*)&val;
+}
+
+unsigned long long& as_ull(double &val)
+{
+    return *(unsigned long long*)&val;
+}
+
+
+FanniRPROP::FanniRPROP()
+{
+}
+
+void FanniRPROP::learn(void * session, std::initializer_list<double> input, std::initializer_list<double> output)
+{
+    assert(output.size() == net.back().nodes.size());
+
+    Session* s = reinterpret_cast<Session*>(session);
+    assert(s->can_learn);
+
+    s->learning = true;
+    uint i = 0;
+    for (auto& in : input)
+        s->expected[i++] = in;
+
+    run(session, std::move(input));
+    s->learning = false;
+}
+
+void FanniRPROP::run(void * session, std::initializer_list<double> input) const
+{
+    assert(input.size() == net.front().nodes.size());
+
+    Session* s = reinterpret_cast<Session*>(session);
+    uint i = 0;
+    for (auto& in : input)
+        s->outputs[0][i++]->store(as_ull(in));
+
+    s->run = true;
+    while (s->run)
+        this_thread::sleep_for(chrono::milliseconds(1));
+}
+
+std::list<double> FanniRPROP::fetchResult(void* session) const
+{
+    list<double> ret;
+
+    const auto& result = reinterpret_cast<Session*>(session)->outputs.back();
+    for (uint i = 0; i < net.back().nodes.size(); ++i)
+        ret.push_back(as_double(result[i]->load()));
+
+    return ret;
+}
+
+std::list<double> FanniRPROP::calc(void * session, std::initializer_list<double> input) const
+{
+    run(session, input);
+    return fetchResult(session);
+}
+
+void FanniRPROP::save(std::ostream & out)
+{
+    auto layers = net.size();
+    out.write((const char*)&layers, sizeof(layers));
+
+    for (Layer& l : net)
+    {
+        auto size = l.nodes.size();
+        out.write((const char*)&l.id, sizeof(l.id));
+        out.write((const char*)&size, sizeof(size));
+        for (Node& n : l.nodes)
+        {
+            out.write((const char*)&n.id, sizeof(n.id));
+            out.write((const char*)&n.layer, sizeof(n.layer));
+            
+            auto weights = n.in_weights.size();
+            out.write((const char*)&weights, sizeof(weights));
+            out.write((const char*)n.in_weights.data(), sizeof(n.in_weights.front())*weights);
+        }
+    }
+}
+
+void FanniRPROP::load(std::istream & in)
+{
+    decltype(net.size()) layers;
+    in.read((char*)&layers, sizeof(layers));
+
+    net.resize(layers);
+    for (Layer& l : net)
+    {
+        decltype(l.nodes.size()) size;
+        in.read((char*)&l.id, sizeof(l.id));
+        in.read((char*)&size, sizeof(size));
+
+        l.nodes.resize(size);
+        for (Node& n : l.nodes)
+        {
+            in.read((char*)&n.id, sizeof(n.id));
+            in.read((char*)&n.layer, sizeof(n.layer));
+
+            decltype(n.in_weights.size()) weights;
+            in.read((char*)&weights, sizeof(weights));
+            n.in_weights.resize(weights);
+            in.read((char*)n.in_weights.data(), sizeof(n.in_weights.front())*weights);
+        }
+    }
+}
+
+void FanniRPROP::createNewNet(initializer_list<uint> const& layers)
 {
     assert(layers.size() >= 2);
     net.clear();
     net.resize(layers.size());
 
-    max_nodes = *max(layers.begin(), layers.end());
+    max_nodes = max(layers);
 
     uint lidx = 0;
     auto itr = layers.begin();
@@ -39,25 +154,6 @@ void FanniRPROP::createNewNet(initializer_list<uint> const & layers)
     }
 }
 
-const double& as_double(unsigned long long const& mem)
-{
-    return *(const double* const)&mem;
-}
-
-double& as_double(unsigned long long& mem)
-{
-    return *(double* const)&mem;
-}
-
-const unsigned long long& as_ull(double const& val)
-{
-    return *(const unsigned long long*)&val;
-}
-
-unsigned long long& as_ull(double &val)
-{
-    return *(unsigned long long*)&val;
-}
 
 void atomic_double_add(atomic_ullong& mem, double value_to_add)
 {
@@ -68,7 +164,7 @@ void atomic_double_add(atomic_ullong& mem, double value_to_add)
     {
         expected = as_double(mem.load());
         sum = expected + value_to_add;
-    } while (mem.compare_exchange_strong(as_ull(expected), as_ull(sum)) != as_ull(expected));
+    } while (mem.compare_exchange_strong(as_ull(expected), as_ull(sum)) == false);
 }
 
 void wait_for_count(atomic_uint& counter, unsigned count)
@@ -93,12 +189,13 @@ int sign(double f)
         return 0;
 }
 
-FanniRPROP::Session* FanniRPROP::initSession()
+FanniRPROP::Session* FanniRPROP::initSession() const
 {
-    Session* new_session = new Session;
+    Session* new_session = new Session(net.size(), max_nodes);
     new_session->exists = true;
     new_session->run = false;
     new_session->learning = false;
+    new_session->can_learn = false;
     atomic_init(&new_session->working, 0);
     atomic_init(&new_session->waiting_counter, 0);
 
@@ -108,19 +205,33 @@ FanniRPROP::Session* FanniRPROP::initSession()
     new_session->delta_min = 0.001;
     new_session->error = 0.0;
     new_session->preverror = 0.0;
-    new_session->outputs.resize(net.size());
-    new_session->d.resize(net.size());
-    new_session
-    new_session->outputs.resize(net.size());
+
     for (uint i = 0; i < net.size(); ++i)
     {
-        new_session->outputs[i].resize(net[i].nodes.size());
-        for (auto& a : new_session->outputs[i])
-            atomic_init(&a, as_ull(0.0));
+        for (uint j = 0; j < max_nodes; ++j)
+        {
+            new_session->outputs[i][j] = new atomic_ullong();
+            new_session->d[i][j] = new atomic_ullong();
+            atomic_init(new_session->outputs[i][j], 0);
+            atomic_init(new_session->d[i][j], 0);
+        }
     }
+
+    new_session->learn_rate_pos = 1.2;
+    new_session->learn_rate_neg = 0.5;
+    new_session->delta_max = 50;
+    new_session->delta_min = 0;
 
     uint wid = 0;
     new_session->workers.resize(max_nodes*max_nodes);
+
+    return new_session;
+}
+
+void FanniRPROP::_initLearningWorkers(Session* new_session)
+{
+    new_session->can_learn = true;
+    uint wid = 0;
     for (auto* tptr : new_session->workers)
     {
         tptr = new thread([new_session, this](uint myneuron, uint prevneuron) {
@@ -133,16 +244,16 @@ FanniRPROP::Session* FanniRPROP::initSession()
 
                 for (uint i = 1; i < net.size(); ++i)
                 {
-                    double tmp = new_session->outputs[i - 1][prevneuron];
-                    atomic_double_add(new_session->outputs[i][myneuron], tmp);
+                    double tmp = as_double(new_session->outputs[i - 1][prevneuron]->load());
+                    atomic_double_add(*new_session->outputs[i][myneuron], tmp);
 
                     wait_for_count(new_session->waiting_counter, new_session->workers.size());
 
                     if (myneuron == 0)
                     {
-                        auto mem = new_session->outputs[i][myneuron].load();
+                        auto mem = new_session->outputs[i][myneuron]->load();
                         auto val = 1.0 / exp(-as_double(mem));
-                        new_session->outputs[i][myneuron] = as_ull(val);
+                        new_session->outputs[i][myneuron]->store(as_ull(val));
                     }
 
                     wait_for_count(new_session->waiting_counter, new_session->workers.size());
@@ -153,27 +264,27 @@ FanniRPROP::Session* FanniRPROP::initSession()
                     auto nextneuron = prevneuron;
 
                     if (nextneuron == 0)
-                        new_session->d.back()[myneuron] = as_ull((new_session->outputs.back()[myneuron] - new_session->expected[myneuron]) * as_double(new_session->outputs.back()[myneuron]) *(1 - as_double(new_session->outputs.back()[myneuron])));
+                        new_session->d.back()[myneuron]->store(as_ull((new_session->outputs.back()[myneuron]->load() - new_session->expected[myneuron]) * as_double(new_session->outputs.back()[myneuron]->load()) *(1 - as_double(new_session->outputs.back()[myneuron]->load()))));
 
                     wait_for_count(new_session->waiting_counter, new_session->workers.size());
 
                     for (int lid = net.size() - 2; lid >= 0; ++lid)
                     {
-                        double tmp = as_double(new_session->d[lid+1][nextneuron].load()) * net[lid+1].nodes[nextneuron].in_weights[myneuron];
-                        atomic_double_add(new_session->d[lid][myneuron], tmp);
+                        double tmp = as_double(new_session->d[lid + 1][nextneuron]->load()) * net[lid + 1].nodes[nextneuron].in_weights[myneuron];
+                        atomic_double_add(*new_session->d[lid][myneuron], tmp);
                         wait_for_count(new_session->waiting_counter, new_session->workers.size());
                     }
 
-                    for (int lid = 1; lid<net.size() - 1; ++lid)
+                    for (uint lid = 1; lid<net.size() - 1; ++lid)
                     {
-                        auto& d = new_session->d[lid][nextneuron];
+                        auto& d = *new_session->d[lid][nextneuron];
                         auto& pdE = new_session->pdE[lid][myneuron][prevneuron];
                         auto& dE = new_session->dE[lid][myneuron][prevneuron];
                         auto& delta = new_session->delta[lid][myneuron][prevneuron];
                         auto& deltaw = new_session->deltaw[lid][myneuron][prevneuron];
                         auto& w = net[lid].nodes[myneuron].in_weights[prevneuron];
 
-                        dE = d * as_double(new_session->outputs[lid-1][myneuron]);
+                        dE = d * as_double(new_session->outputs[lid - 1][myneuron]->load());
                         wait_for_count(new_session->waiting_counter, new_session->workers.size());
 
                         if (pdE*dE > 0)
@@ -198,11 +309,66 @@ FanniRPROP::Session* FanniRPROP::initSession()
 
                 }
 
-                new_session->working.fetch_sub(1);
+                if (new_session->working.fetch_sub(1) == 1)
+                    new_session->run = false;
+                else
+                    while (new_session->working.load() != 0)
+                    this_thread::sleep_for(chrono::milliseconds(100));
 
             }
-        }, wid/max_nodes, wid%max_nodes);
-    }
+        }, wid / max_nodes, wid%max_nodes);
 
-    return new_session;
+        ++wid;
+    }
+}
+
+void FanniRPROP::_initRunningWorkers(Session* new_session) const
+{
+    uint wid = 0;
+    for (auto* tptr : new_session->workers)
+    {
+        tptr = new thread([new_session, this](uint myneuron, uint prevneuron) {
+            while (new_session->exists)
+            {
+                while (!new_session->run)
+                    this_thread::sleep_for(chrono::milliseconds(100));
+
+                new_session->working.fetch_add(1);
+
+                for (uint i = 1; i < net.size(); ++i)
+                {
+                    double tmp = as_double(new_session->outputs[i - 1][prevneuron]->load());
+                    atomic_double_add(*new_session->outputs[i][myneuron], tmp);
+
+                    wait_for_count(new_session->waiting_counter, new_session->workers.size());
+
+                    if (myneuron == 0)
+                    {
+                        auto mem = new_session->outputs[i][myneuron]->load();
+                        auto val = 1.0 / exp(-as_double(mem));
+                        new_session->outputs[i][myneuron]->store(as_ull(val));
+                    }
+
+                    wait_for_count(new_session->waiting_counter, new_session->workers.size());
+                }
+
+                if (new_session->working.fetch_sub(1) == 1)
+                    new_session->run = false;
+                else
+                    while (new_session->working.load() != 0)
+                        this_thread::sleep_for(chrono::milliseconds(100));
+            }
+        }, wid / max_nodes, wid%max_nodes);
+
+        ++wid;
+    }
+}
+
+
+FanniRPROP::Session::Session(uint layers, uint nodes) : outputs(nullptr, layers, nodes), d(nullptr, layers, nodes), dE(layers, nodes, nodes), pdE(layers, nodes, nodes), delta(0.5, layers, nodes, nodes), deltaw(layers, nodes, nodes), expected(nodes)
+{
+}
+
+FanniRPROP::Session::~Session()
+{
 }
--- src/ann/fann_irprop_logical.h
+++ src/ann/fann_irprop_logical.h
@@ -5,16 +5,46 @@
 #include <vector>
 #include <atomic>
 #include <thread>
+#include <iostream>
 #include <initializer_list>
 
 template <typename T, uint DIM>
-struct vector_n : public std::vector<vector_n<T, DIM-1>>
+struct vector_n : public std::vector<vector_n<T, DIM - 1>>
 {
+    vector_n() : std::vector<vector_n<T, DIM - 1>>()
+    {}
+
+    template <typename... INTS>
+    vector_n(T val, uint mysize, INTS... rest) : std::vector<vector_n<T, DIM-1>>(mysize, vector_n<T, DIM-1>(val, rest...))
+    {
+        static_assert(_all_same<uint, INTS...>::value, "");
+        static_assert(sizeof...(rest) == DIM-1, "");
+    }
+
+    template <typename... INTS>
+    vector_n(uint mysize, INTS... rest) : std::vector<vector_n<T, DIM - 1>>(mysize, vector_n<T, DIM - 1>(rest...))
+    {
+        static_assert(_all_same<uint, INTS...>::value, "");
+        static_assert(sizeof...(rest) == DIM - 1, "");
+    }
+
+private:
+    template <typename TEST, typename T1, typename... REST> struct _all_same { enum { value = _all_same<TEST, T1>::value && _all_same<TEST, REST...>::value }; };
+    template <typename TEST, typename A> struct _all_same<TEST,A>{ enum { value = false }; };
+    template <typename TEST> struct _all_same<TEST,TEST>{ enum { value = true }; };
 };
 
 template <typename T>
 struct vector_n<T, 1> : public std::vector<T>
 {
+    vector_n() : std::vector<T>()
+    {}
+
+    vector_n(T val, uint mysize) : std::vector<T>(mysize, val)
+    {}
+
+    vector_n(uint mysize) : std::vector<T>(mysize)
+    {}
 };
 
 template <typename T>
@@ -25,12 +55,23 @@ struct vector_n<T, 0>
 class FanniRPROP
 {
 public:
+    FanniRPROP();
     FanniRPROP(std::initializer_list<uint> const& layers);
     ~FanniRPROP();
 
-    void* createSession();
+    inline void* createSession() const { auto* ret = initSession(); _initRunningWorkers(ret); return ret; }
+    inline void* createLearningSession() { auto* ret = initSession(); _initLearningWorkers(ret); return ret; }
 
     void learn(void* session, std::initializer_list<double> input, std::initializer_list<double> output);
+
+    void run(void* session, std::initializer_list<double> input) const;
+    std::list<double> fetchResult(void* session) const;
+
+    //run && fetchResult
+    std::list<double> calc(void* session, std::initializer_list<double> input) const;
+
+    void save(std::ostream& out);
+    void load(std::istream& in);
 
 private:
     struct Node
@@ -53,8 +94,11 @@ private:
 
     struct Session
     {
-        vector_n<std::atomic_ullong, 2> outputs;
-        vector_n<std::atomic_ullong, 2> d;
+        Session(uint layers, uint nodes);
+        ~Session();
+
+        vector_n<std::atomic_ullong*, 2> outputs;
+        vector_n<std::atomic_ullong*, 2> d;
         vector_n<double, 3> dE; //pochodne bledu po wadze, uzywane przy nauce
         vector_n<double, 3> pdE; //pochodne bledu po wadze, uzywane przy nauce
         vector_n<double, 3> delta;
@@ -65,6 +109,7 @@ private:
         bool exists;
         bool run;
         bool learning;
+        bool can_learn;
         std::atomic_int working;
         std::atomic_uint waiting_counter;
 
@@ -77,5 +122,8 @@ private:
         vector_n<double, 1> expected;
     };
 
-    Session* initSession();
+    Session* initSession() const;
+
+    void _initLearningWorkers(Session* s);
+    void _initRunningWorkers(Session* s) const;
 };
